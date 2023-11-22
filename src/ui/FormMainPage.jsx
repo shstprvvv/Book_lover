a58434a453bc28178a4c26af09d69a78777d3e1f
viktoria <viktoria_19_1@mail.ru>
2023-11-22 15:12:48 +0300

    ui

--- src/ui/FormMainPage.jsx
+++ src/ui/FormMainPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function BasicExample() {
+function FormMainPage({book}) {
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="" />
@@ -16,4 +16,4 @@ function BasicExample() {
   );
 }
 
-export default BasicExample;
+export default FormMainPage;
