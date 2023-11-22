7f05236d33ff8e259adfd2d4aa073d6b97719d37
viktoria <viktoria_19_1@mail.ru>
2023-11-22 14:34:19 +0300

    pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import MainPage from './page/MainPage';
+import SignUpPage from './page/SignUpPage';
+import LoginPage from './page/LoginPage';
+import AccountPage from './page/AccountPage';
+import AddBook from './page/AddBook';
+import BookPage from './page/BookPage';
 
-export default function App({ hello }) {
-  return <div>Hello, {hello}</div>;
+export default function App() {
+  return (
+    <div className="container">
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/account" element={<AccountPage />} />
+        <Route path="/addbook" element={<AddBook />} />
+        <Route path="/bookpage" element={<BookPage />} />
+      </Routes>
+    </div>
+  )
 }
